5d2445323e18cd57ab6287c8a41d787618fddf2c
nidanasir <nasirn4@mcmaster.ca>
2024-06-24 15:31:16 -0400

    added all assets and pages

---
+++ assets/js/darkmode.js
@@ -0,0 +1,10 @@
+/*
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
