aa72a7cf0eb252283c6d898833e755d992610549
nidanasir <nasirn4@mcmaster.ca>
2024-07-16 15:37:44 -0400

    added personal photo and fixed dark mode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,10 +1,18 @@
-/*
-	Phantom by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+function toggleDarkMode() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+	if (element.classList.contains("dark-mode")) {
+		localStorage.setItem("darkMode", "enabled");
+	} else {
+		localStorage.setItem("darkMode", "disabled");
+	}
 }
+
+function applyDarkModePreference() {
+	if (localStorage.getItem("darkMode") === "enabled") {
+		document.body.classList.add("dark-mode");
+	}
+}
+
+// Apply the user's dark mode preference on page load
+applyDarkModePreference();
